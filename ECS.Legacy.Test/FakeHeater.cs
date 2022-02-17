a0f7661e689c64f92905cead1b503c0af565adc5
camil <camil@DESKTOP-8M3PEC4>
2022-02-17 13:00:02 +0100

    Oprettet FakeTestKlasser

---
+++ ECS.Legacy.Test/FakeHeater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECS.Refactored;
+
+namespace ECS.Legacy.Test
+{
+   public class FakeHeater : IHeater
+
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("FakeHeater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("FakeHeater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
